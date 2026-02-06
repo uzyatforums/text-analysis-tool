64d35cd6b88704ee25413367548985b9c2470320
Uzy Nwosu <uzyatforums@gmail.com>
2026-02-06 04:45:18 +0100

    got input from user and displayed it

---
+++ analyze.py
@@ -0,0 +1,4 @@
+print("Welcome to the text analysis tool. I will mine and analyse a body of text from a file you give me")
+
+username = input("Enter your username: ")
+print(f"Welcome, {username}!")
