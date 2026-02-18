746888a3703035d17871fea06047077177806476
Victor Nwosu <uzyatforums@gmail.com>
2026-02-18 07:35:19 +0100

    converted input prompt to function

--- analyze.py
+++ analyze.py
@@ -1,4 +1,8 @@
-print("Welcome to the text analysis tool. I will mine and analyse a body of text from a file you give me")
+def welcomeUser():
+    print("Welcome to the text analysis tool. I will mine and analyse a body of text from a file you give me")
 
+welcomeUser()
 username = input("Enter your username: ")
 print(f"Welcome, {username}!")
+
+
