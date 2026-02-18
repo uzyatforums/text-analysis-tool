e485f9bdf02ab82b19c3019907302cbdf2558573
Victor Nwosu <uzyatforums@gmail.com>
2026-02-18 07:42:31 +0100

    Introduced getusername function

--- analyze.py
+++ analyze.py
@@ -1,8 +1,13 @@
 def welcomeUser():
     print("Welcome to the text analysis tool. I will mine and analyse a body of text from a file you give me")
 
+
+def getusername():
+    username = input("Enter your username: ")
+    return username
+
 welcomeUser()
-username = input("Enter your username: ")
+username = getusername()
 print(f"Welcome, {username}!")
 
 
