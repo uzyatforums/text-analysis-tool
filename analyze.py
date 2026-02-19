a337dda66970a4f3e6f130ca655ab35ae70ff3c7
Victor Nwosu <uzyatforums@gmail.com>
2026-02-19 23:00:20 +0100

    Added username input validation

--- analyze.py
+++ analyze.py
@@ -1,10 +1,24 @@
+from random_username.generate import generate_username
+
 def welcomeUser():
+    # Print message prompting user  to input their name
     print("Welcome to the text analysis tool. I will mine and analyse a body of text from a file you give me")
 
 
 def getusername():
-    username = input("Enter your username: ")
-    return username
+    usernameFromInput = input("\nTo begin, please enter your username:\n")
+
+    usernameLessThan5Chars = len(usernameFromInput) < 5
+
+    print("Less than 5 characters: " + str(usernameLessThan5Chars))
+
+
+    if len(usernameFromInput) < 5 or not usernameFromInput.isidentifier():
+        print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), have no spaces, and cannot start with a number")
+        print("Assigning username instead...")
+        usernameFromInput = generate_username()[0]
+
+    return usernameFromInput
 
 def greetUser(username):
     print(f"Welcome, {username}!")
